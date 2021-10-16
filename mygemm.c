fc94302eae58d1da1ca7c637d9eb3c5ab6d41d0a
andy666la <437965859@qq.com>
2021-10-15 17:20:48 -0700

    Update mygemm.c

--- mygemm.c
+++ mygemm.c
@@ -10,131 +10,26 @@
 //Register Reuse part 1
 void dgemm0(const double* A, const double* B, double* C, const int n)
 {
-	int i,j,k;
-    for (i=0; i<n; i++) 
-        for (j=0; j<n; j++) 
-            for (k=0; k<n; k++) 
-               C[i*n+j] += A[i*n+k] * B[k*n+j];
+
 }
 
 void dgemm1(const double *A, const double *B, double *C, const int n) 
 {
-	 int i,j,k;
-    for (i=0; i<n; i++) 
-        for (j=0; j<n; j++) { 
-            register double r = C[i*n+j] ; 
-        for (k=0; k<n; k++) 
-            r += A[i*n+k] * B[k*n+j]; 
-        C[i*n+j] = r; 
-    }
+
 }
 //Register Reuse part 1 End
-/**
+
 //Register Reuse part 2
 void dgemm2(const double *A, const double *B, double *C, const int n) 
 {
-	int i, j, k;
-    for (i = 0; i < n; i+=2) {
-        for (j = 0; j < n; j+=2) {
-            for (k = 0; k < n; k+=2) {
-                C[i*n + j] = A[i*n + k]*B[k*n + j] + A[i*n + k+1]*B[(k+1)*n + j] + C[i*n + j];
-                C[(i+1)*n + j] = A[(i+1)*n + k]*B[k*n + j] + A[(i+1)*n + k+1]*B[(k+1)*n + j] + C[(i+1)*n + j];
-                C[i*n + (j+1)] = A[i*n + k]*B[k*n + (j+1)] + A[i*n + k+1]*B[(k+1)*n + (j+1)] + C[i*n + (j+1)];
-                C[(i+1)*n + (j+1)] = A[(i+1)*n + k]*B[k*n + (j+1)] + A[(i+1)*n + k+1]*B[(k+1)*n + (j+1)] + C[(i+1)*n + (j+1)];
-            }
-        } 
-    } 
+
 }
 //Register Reuse part 2 End
 
 //Register Reuse part 3
 void dgemm3(const double *A, const double *B, double *C, const int n) 
 {
-	int i, j, k;
-    for (i = 0; i < n; i += 3) {
-        for (j = 0; j < n; j += 3) {
-            register int t = i*n+j; // 00
-            register int tt = t+n; // 01
-            register int ttt = tt+n; // 02
-            register double rc00 = C[t];
-            register double rc01 = C[t+1];
-            register double rc02 = C[t+2];
-            register double rc10 = C[tt];
-            register double rc11 = C[tt+1];
-            register double rc12 = C[tt+2];
-            register double rc20 = C[ttt];
-            register double rc21 = C[ttt+1];
-            register double rc22 = C[ttt+2];
-
-            for (k = 0; k < n; k += 3) {
-                register int ta = i*n+k;
-                register int tta = ta+n;
-                register int ttta = tta+n;
-                register int tb = k*n+j;
-                register int ttb = tb+n;
-                register int tttb = ttb+n;
-
-                register double R1 = A[ta]; // ra00
-                register double R2 = A[tta]; // ra10
-                register double R3 = A[ttta]; // ra20
-                register double R4 = B[tb]; // rb00
-                register double R5 = B[tb+1]; // rb01
-                register double R6 = B[tb+2]; // rb02
-
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-
-                R1 = A[ta+1];
-                R2 = A[tta+1];
-                R3 = A[ttta+1];
-                R4 = B[ttb];
-                R5 = B[ttb+1];
-                R6 = B[ttb+2];
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-
-                R1 = A[ta+2];
-                R2 = A[tta+2];
-                R3 = A[ttta+2];
-                R4 = B[tttb];
-                R5 = B[tttb+1];
-                R6 = B[tttb+2];
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-            }
-            C[t] = rc00;
-            C[t+1] = rc01;
-            C[t+2] = rc02;
-            C[tt] = rc10;
-            C[tt+1] = rc11;
-            C[tt+2] = rc12;
-            C[ttt] = rc20;
-            C[ttt+1] = rc21;
-            C[ttt+2] = rc22;
-        }
-    }
-
+	
 
 }
 //Register Reuse part 3 End
@@ -332,4 +227,3 @@ void optimal(const double* A, const double* B, double *C, const int n, const int
                         C[tt+1] = c11;
                     }
 }
-**/
