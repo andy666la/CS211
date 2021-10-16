a9dce9a77bf73b50dcd6426228ca128430c5bd78
andy666la <437965859@qq.com>
2021-10-15 17:46:23 -0700

    test for q2

--- mygemm.c
+++ mygemm.c
@@ -8,6 +8,32 @@
  **/
 
 //Register Reuse part 1
+void dgemm0(const double* A, const double* B, double* C, const int n)
+{
+
+}
+
+void dgemm1(const double *A, const double *B, double *C, const int n) 
+{
+
+}
+//Register Reuse part 1 End
+
+//Register Reuse part 2
+void dgemm2(const double *A, const double *B, double *C, const int n) 
+{
+	
+}
+//Register Reuse part 2 End
+
+//Register Reuse part 3
+void dgemm3(const double *A, const double *B, double *C, const int n) 
+{
+	
+
+
+}
+//Register Reuse part 3 End
 
 //Cache Reuse part 3
 void ijk(const double *A, const double *B, double *C, const int n) 
@@ -178,5 +204,27 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
 //Cache Reuse part 4
 void optimal(const double* A, const double* B, double *C, const int n, const int b)
 {
-    
+    int i, j, k, i1, j1, k1;
+    for (i = 0; i < n; i+=b)
+        for (j = 0; j < n; j+=b)
+            for (k = 0; k < n; k+=b)
+                
+                for (i1 = i; i1 < i+b; i += 2)
+                    for (j1 = j; j1 < j+b; j += 2) {
+                        register int t = i1*n+j1; register int tt = t+n; 
+                        register double c00 = C[t]; register double c01 = C[t+1];  register double c10 = C[tt]; register double c11 = C[tt+1];
+                        for (k1 = k; k1 < k+b; k += 2) {
+                            
+                            register int ta = i1*n+k1; register int tta = ta+n; register int tb = k1*n+j1; register int ttb = tb+n;
+                            register double a00 = A[ta]; register double a10 = A[tta]; register double b00 = B[tb]; register double b01 = B[tb+1]; 
+
+                            c00 += a00*b00 ; c01 += a00*b01 ; c10 += a10*b00 ; c11 += a10*b01 ;
+                            a00 = A[ta+1]; a10 = A[tta+1]; b00 = B[ttb]; b01 = B[ttb+1];
+                            c00 += a00*b00 ; c01 += a00*b01 ; c10 += a10*b00 ; c11 += a10*b01 ;
+                        }
+                        C[t] = c00;
+                        C[t+1] = c01;
+                        C[tt] = c10;
+                        C[tt+1] = c11;
+                    }
 }
