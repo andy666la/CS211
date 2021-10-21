f0c138b8a063d6ed454fdce243c6107f959b7b1f
andy666la <437965859@qq.com>
2021-10-21 02:38:49 -0700

    Update mygemm.c

--- mygemm.c
+++ mygemm.c
@@ -46,93 +46,30 @@ int i=0;int j=0;int k=0;
 //Register Reuse part 2 End
 
 //Register Reuse part 3
-void dgemm3(const double *A, const double *B, double *C, const int n) 
+void dgemm3(const double *A, const double *B, double *C, const int n)
 {
-	int i, j, k;
-    for (i = 0; i < n; i += 3) {
-        for (j = 0; j < n; j += 3) {
-            register int t = i*n+j; // 00
-            register int tt = t+n; // 01
-            register int ttt = tt+n; // 02
-            register double rc00 = C[t];
-            register double rc01 = C[t+1];
-            register double rc02 = C[t+2];
-            register double rc10 = C[tt];
-            register double rc11 = C[tt+1];
-            register double rc12 = C[tt+2];
-            register double rc20 = C[ttt];
-            register double rc21 = C[ttt+1];
-            register double rc22 = C[ttt+2];
+        int i,j,k;
+for(i = 0; i < n; i += 2)
+       for(j = 0; j < n; j += 2)  {
+           register int t = i*n+j; register int tt = t+n;
+           register double c00 = C[i*n+j]; register double c01 = C[i*n+j +1];  register double c10 = C[tt]; register double c11 = C[tt+1];
 
-            for (k = 0; k < n; k += 3) {
-                register int ta = i*n+k;
-                register int tta = ta+n;
-                register int ttta = tta+n;
-                register int tb = k*n+j;
-                register int ttb = tb+n;
-                register int tttb = ttb+n;
+            for(k = 0; k < n; k += 2) {
+                /* 2 by 2 mini matrix multiplication using registers*/
+                register int ta = i*n+k; register int tta = ta+n; register int tb = k*n+j; register int ttb = tb+n;
+                register double a00 = A[ta]; register double a01 = A[ta+1]; register double a10 = A[tta]; register double a11 = A[tta+1];
+                register double b00 = B[tb]; register double b01 = B[tb+1]; register double b10 = B[ttb]; register double b11 = B[ttb+1];
+                c00 += a00*b00 + a01*b10;
+                c01 += a00*b01 + a01*b11;
+                c10 += a10*b00 + a11*b10;
+                c11 += a10*b01 + a11*b11;
+             }
 
-                register double R1 = A[ta]; // ra00
-                register double R2 = A[tta]; // ra10
-                register double R3 = A[ttta]; // ra20
-                register double R4 = B[tb]; // rb00
-                register double R5 = B[tb+1]; // rb01
-                register double R6 = B[tb+2]; // rb02
-
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-
-                R1 = A[ta+1];
-                R2 = A[tta+1];
-                R3 = A[ttta+1];
-                R4 = B[ttb];
-                R5 = B[ttb+1];
-                R6 = B[ttb+2];
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-
-                R1 = A[ta+2];
-                R2 = A[tta+2];
-                R3 = A[ttta+2];
-                R4 = B[tttb];
-                R5 = B[tttb+1];
-                R6 = B[tttb+2];
-                rc00 += R1 * R4;
-                rc01 += R1 * R5;
-                rc02 += R1 * R6;
-                rc10 += R2 * R4;
-                rc11 += R2 * R5;
-                rc12 += R2 * R6;
-                rc20 += R3 * R4;
-                rc21 += R3 * R5;
-                rc22 += R3 * R6;
-            }
-            C[t] = rc00;
-            C[t+1] = rc01;
-            C[t+2] = rc02;
-            C[tt] = rc10;
-            C[tt+1] = rc11;
-            C[tt+2] = rc12;
-            C[ttt] = rc20;
-            C[ttt+1] = rc21;
-            C[ttt+2] = rc22;
+             C[t] = c00;
+             C[t+1] = c01;
+             C[tt] = c10;
+             C[tt+1] = c11;
         }
-    }
-
 
 }
 //Register Reuse part 3 End
@@ -304,11 +241,14 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
 {
 int i1,j1,k1;
 int i,j,k;
-	for (k1 = k; k1 < k+b; k1++)
-		for (j1 = j; j1 < j+b; j1++) {
-		register double r=B[k1*n+j1];
-			for (i1 = i; i1 < i+b; i1++)
-			C[i1*n+j1] += A[i1*n + k1]*r;
+	for (k = 0; k < n; k+=b)
+		for (i = 0; i < n; i+=b)
+			for (j = 0; j < n; j+=b)
+				for (k1 = k; k1 < k+b; k1++)
+					for (j1 = j; j1 < j+b; j1++) {
+					register double r=B[k1*n+j1];
+						for (i1 = i; i1 < i+b; i1++)
+						C[i1*n+j1] += A[i1*n + k1]*r;
 }
 
 }
