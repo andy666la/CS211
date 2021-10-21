2d86e82be1b6b1eac3e23677c95197cd72801d49
andy666la <437965859@qq.com>
2021-10-21 02:50:30 -0700

    Update mygemm.c

--- mygemm.c
+++ mygemm.c
@@ -75,16 +75,16 @@ for(i = 0; i < n; i += 2)
 //Register Reuse part 3 End
 
 //Cache Reuse part 3
-void ijk(const double *A, const double *B, double *C, const int n) 
-{	
-	  int i, j, k;
-    for (i=0; i<n; i++) 
-        for (j=0; j<n; j++) { 
-            register double r=C[i*n+j];
-            for (k=0; k<n; k++) 
-                r += A[i*n+k] * B[i*k+j]; 
-            C[i*n+j] = r;
-}
+void ijk(const double *A, const double *B, double *C, const int n)
+{
+	int i,j,k;
+	for (i=0; i<n; i++)
+		for (j=0; j<n; j++){
+			register double r=C[i*n+j];
+				for (k=0; k<n; k++)
+				r += A[i*n+k] * B[k*n+j];
+				C[i*n+j]=r;
+			}
 }
 
 void bijk(const double *A, const double *B, double *C, const int n, const int b)
